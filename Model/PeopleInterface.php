2c76cb218ffcc9a9aec51d309f5b17c7055c3d05
AlexeySadovsky <alexei.s@phpdev.org>
2022-08-05 14:33:15 +0300

    task

--- Model/PeopleInterface.php
+++ Model/PeopleInterface.php
@@ -1,11 +1,25 @@
 <?php
 
+/**
+* Автор: Садовский Алексей
+*
+* Дата реализации: 05.08.2022 10:00
+*
+* Дата изменения: 05.08.2022 10:20
+*
+* Интерфейс для класса работы с базой данных
+*
+*/
+
 Interface PeopleInterface
 {
 
     public function __construct();
+
     public function input();
+
     public function check();
+
     public function delete($id);
 
 }
